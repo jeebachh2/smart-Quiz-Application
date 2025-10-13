8a6ece327396cba24d4063938f72862a95b9f0db
jeebachh kumar <jeebachhk69@gmail.com>
2025-10-13 19:01:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,161 @@
+let startBtn = document.querySelector('.startBtn');
+let infoBox = document.querySelector('.infoBox');
+let exitBtn = document.querySelector('.exitBtn');
+let continueBtn = document.querySelector('.continueBtn');
+let quizBox = document.querySelector('.QuizBox');
+let questionText = document.querySelector('.questiontext');
+let allOptions = document.querySelectorAll('.option');
+let nextBtn = document.querySelector('.nextbtn');
+let timeline = document.querySelector('.timeline');
+let currentQuestionIndicator = document.querySelector('.currentQuestionIndicator');
+let progressBar = document.querySelector('.progressBar');
+let timeLineTitle = document.querySelector('.time-line-title');
+let currentQuestionIndex = 0;
+let userScore = 0;
+
+let timeLineInterval = null;
+let progressBarInterval = null;
+
+const TicIcon = '<div class="icon tick"><i class="fa-solid fa-check"></i></div>';
+const crossIcon = '<div class="icon cross"><i class="fa-solid fa-xmark"></i></div>';
+
+startBtn.addEventListener('click', () => {
+    // we have to inject a class name to infoBox
+    infoBox.classList.add('activeInfoBox');
+    
+});
+
+exitBtn.addEventListener('click', () => {
+    infoBox.classList.remove('activeInfoBox');
+});
+
+continueBtn.addEventListener('click', () => {
+    infoBox.classList.remove('activeInfoBox');
+    quizBox.classList.add('activeQuizBox');
+    // here we need to start showing question
+    showQuestion(currentQuestionIndex);
+    handleTiming(15);
+    handleprogressBar();
+    timeLineTitle.innerText = 'Time left';
+});
+
+nextBtn.addEventListener('click', () => {
+    if(currentQuestionIndex <9) {
+        currentQuestionIndex = currentQuestionIndex + 1;
+
+        // reset the time
+        // reset the progress bar
+        handleTiming(15);
+        handleprogressBar();
+
+    showQuestion(currentQuestionIndex);
+    nextBtn.classList.remove('active');
+    timeLineTitle.innerText = '.Time Left';
+    }
+});
+
+// function to show/render questions
+const showQuestion = (index) => {
+    questionText.innerText = '' + questions?.[index].numb +'. ' + questions?.[index].question;  
+
+   for(let i=0; i<allOptions?.length; i++) {
+    allOptions[i].innerText = questions?.[index].options?.[i];
+    allOptions[i].classList.remove('correct');
+    allOptions[i].classList.remove('incorrect');
+    allOptions[i].classList.remove('disabled');
+    
+    if(index === 0) {
+        allOptions[i]?.addEventListener('click', optionClickHandle);
+    }
+   }
+
+   currentQuestionIndicator.innerText = index + 1;
+};
+
+const handleTiming = (time) => {
+    clearInterval(timeLineInterval);
+    timeline.innerText = time;
+    let timeValue = time;
+    timeLineInterval = setInterval(() => {
+        timeValue--;
+
+        if(timeValue < 10) {
+            timeline.innerText = '0' + timeValue;
+        } else {
+            timeline.innerText = timeValue;
+        }
+        if(timeValue===0) {
+            //you cross the time
+            timeLineTitle.innerText = 'Time off';
+            clearInterval(timeLineInterval);
+            // make next button visible
+            nextBtn.classList.add('active');
+            // display correct answer
+            const correctAnswer = questions[currentQuestionIndex].answer;
+            for(let i=0; i<allOptions?.length; i++) {
+            allOptions[i].classList.add('disabled');
+
+            if( allOptions[i].innerText === correctAnswer) {
+                 allOptions[i].classList.add('correct');
+                allOptions[i].insertAdjacentHTML('beforeend', TicIcon);
+            }
+     }
+
+        }
+
+    }, 1000);
+
+}
+
+const handleprogressBar = () => {
+    clearInterval(progressBarInterval);
+    progressBar.style.width = '0%';
+    let currentPercentage = 0;
+    progressBarInterval  = setInterval (() => {
+        currentPercentage += 1/15;
+        progressBar.style.width = currentPercentage + '%';
+
+        if(currentPercentage >= 100) {
+            clearInterval(progressBarInterval);
+        }
+    }, 10);
+};
+
+const optionClickHandle = (e) => {
+    console.log(e);
+    clearInterval(progressBarInterval);
+    clearInterval(timeLineInterval);
+    nextBtn.classList.add('active');
+    const userAnswer = e.target.innerText;
+    const correctAnswer = questions[currentQuestionIndex].answer;
+    console.log(correctAnswer, userAnswer);
+
+    if(userAnswer === correctAnswer) {
+        userScore++;
+        e.target.classList.add('correct');
+        e.target.insertAdjacentHTML('beforeend', TicIcon);
+    } else {
+        // wrong answer
+        // mark user response as wrong
+        // traverse and find correct answer
+        // and then mark it as correct
+        e.target.classList.add('incorrect');
+        e.target.insertAdjacentHTML('beforeend', crossIcon);
+
+         for(let i=0; i<allOptions?.length; i++) {
+            if(allOptions[i].innerText === correctAnswer) {
+                
+            } 
+        }
+    }
+
+     for(let i=0; i<allOptions?.length; i++) {
+            allOptions[i].classList.add('disabled');
+
+            if(userAnswer !== correctAnswer && allOptions[i].innerText === correctAnswer) {
+                 allOptions[i].classList.add('correct');
+                allOptions[i].insertAdjacentHTML('beforeend', TicIcon);
+            }
+     }
+
+};
---
+++ question.js
@@ -0,0 +1,62 @@
+let questions = [
+   {
+     numb: 1.,
+    question: 'which type of language is javascript?',
+    answer: "scripting",
+    options: ['programing', 'scripting', 'markup', 'styling'],
+   },
+   {
+     numb: 2,
+    question: 'which keyword is used to declare a variable in javascript?',
+    answer: "All of the above",
+    options: ['var', 'let', 'const', 'All of the above'],
+   },
+   {
+     numb: 3,
+    question: 'which built-in method is used to parse a string to an integer?',
+    answer: "parseInt()",
+    options: ['parseInt()', 'parseFloat()', 'toFixed()', 'toPrecision()'],
+   },
+   {
+     numb: 4,
+    question: 'which keyword is used to declare a variable in javascript',
+    answer: "All of the above",
+    options: ['var', 'let', 'const', 'All of the above'],
+   },
+   {
+    numb: 5,
+    question: 'which of the following is not a javascript framework library?',
+    answer: "Laravel",
+    options: ['React', 'angular', 'Vue', 'Laravel'],
+   },
+   {
+    numb: 6,
+    question: 'which keyword is used to declare a variable in javascript',
+    answer: "All of the above",
+    options: ['var', 'let', 'const', 'All of the above'],
+   },
+   {
+    numb: 7,
+    question: 'which method is used to add an element at the end of an array?',
+    answer: "push()",
+    options: ['push()', 'pop()', 'shift()', 'unshift()'],
+   },
+   {
+    numb: 8,
+    question: "what does the 'this' keyword refer to in javascript ",
+    answer: "Depends on the context",
+    options: ['Global object', 'Current function', 'Depends on the context', 'window object'],
+   },
+  {
+    numb: 9,
+    question: 'which Javascript method is used to fetch data from an API?.',
+    answer: "fetch()",
+    options: ['get()', 'fetch()', 'retrieve()', 'httpRequest()'],
+  },
+   {
+    numb: 10,
+    question: 'How do you declare an astnchoronous function in javascript',
+    answer: "async function()",
+    options: ['async function()', 'function async()', 'async: function()', 'await function()'],
+   }
+]
